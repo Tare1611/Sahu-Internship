3c4de17c4483998cce64f8ff368a3aeea58f66c4
Shashwat Tare <53914257+Tare1611@users.noreply.github.com>
2021-05-17 20:03:11 +0530

    Major Changes in Code
    Extra files removed

---
+++ Web Dev Template 1/contact/js/main.js
@@ -0,0 +1,75 @@
+/* ===============================================
+    1. Scrolling Top
+    2. Contact Form
+  ================================================== */
+
+
+/* ===============================================
+    Function Call - Call Function Ones
+   =============================================== */
+
+jQuery(document).ready(function(){
+	"use strict";
+	scroll_top();
+    testimonial_carousel();
+});
+
+/* ===============================================
+    1. Scrolling Top - Button to scroll up
+   =============================================== */
+function scroll_top(){
+    "use strict";
+	var offset = 300,
+		offset_opacity = 1200,
+		scroll_top_duration = 700,
+		$back_to_top = $('.cd-top');
+
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
+};
+
+/* ===============================================
+    2. Contact Form - This is used for the contact form
+   =============================================== */
+"use strict";
+$(function () {
+
+    $('#contact-form').validator();
+
+    $('#contact-form').on('submit', function (e) {
+        if (!e.isDefaultPrevented()) {
+            var url = "contact.php";
+
+            $.ajax({
+                type: "POST",
+                url: url,
+                data: $(this).serialize(),
+                success: function (data)
+                {
+                    var messageAlert = 'alert-' + data.type;
+                    var messageText = data.message;
+
+                    var alertBox = '<div class="alert ' + messageAlert + ' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' + messageText + '</div>';
+                    if (messageAlert && messageText) {
+                        $('#contact-form').find('.messages').html(alertBox);
+                        $('#contact-form')[0].reset();
+                    }
+                }
+            });
+            return false;
+        }
+    })
+});
